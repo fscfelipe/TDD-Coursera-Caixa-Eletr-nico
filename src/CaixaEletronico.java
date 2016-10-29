80812503e45961295bfbcc8334c9fba6765a8f3c
fscfelipe <felipesobreira.c@hotmail.com>
2016-10-29 00:11:50 -0300

    Criado escopo do projeto #2

---
+++ src/CaixaEletronico.java
@@ -0,0 +1,27 @@
+public class CaixaEletronico {
+
+	public String logar() {
+		return "Usuário Autenticado";
+
+	}
+
+	public String sacar() {
+		
+		if (true)
+			return "Retire seu dinheiro";
+		else
+			return "Saldo insuficiente";
+		
+	}
+
+	public String depositar() {
+		return "Depósito recebido com sucesso";
+
+	}
+
+	public String saldo() {
+		return "O saldo é R$xx,xx";
+
+	}
+
+}
