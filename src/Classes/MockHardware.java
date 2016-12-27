ace915597e109b679e58f1545e60f7edd62ecdf5
fscfelipe <felipesobreira.c@hotmail.com>
2016-12-27 08:15:43 -0300

    Criado mock para Hardware

---
+++ src/Classes/MockHardware.java
@@ -0,0 +1,23 @@
+package Classes;
+
+public class MockHardware implements Hardware {
+
+	@Override
+	public String pegarNumeroDaContaCartao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void entregarDinheiro() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void lerEnvelope() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
